76074c123495555b548f2886b70a504fc9b0dc4d
fadak <f9999cat.6.29kk@gmail.com>
2024-10-10 01:17:30 +0300

    home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,6 +10,14 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return  const Scaffold();
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 176, 126, 191),
+          Color.fromARGB(255, 255, 255, 255),
+        ], begin: Alignment.topRight, end: Alignment.center)),
+      ),
+    );
   }
 }
